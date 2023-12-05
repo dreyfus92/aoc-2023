b9e52b7eec4b87ad2388a240a78b5cc84f30a015
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-12-05 12:49:17 -0600

    add: add template

---
+++ days/template/code.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	p1 := Part01()
+	p2 := Part02()
+	fmt.Println("Part1 Answer:", p1)
+	fmt.Println("Part2 Answer:", p2)
+}
+
+func Part01() int {
+	return 0
+}
+
+func Part02() int {
+	return 0
+}
